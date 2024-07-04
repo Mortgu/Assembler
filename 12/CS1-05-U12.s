f838819ef0c922aae1b9a4a4e823611e9d1f24f5
Oskar Sammet <sao41449@im-lan-282.hs-regensburg.de>
2024-07-04 11:18:08 +0200

    save 12

--- 12/CS1-05-U12.s
+++ 12/CS1-05-U12.s
@@ -1,42 +1,50 @@
 .section .rodata
-format:
-.asciz "Sum = %ld\n"
+    format:
+    .asciz "Sum = %ld\n"
 .section .bss
 .lcomm results 16
 .section .data
-.align 16
-farr1:
-.float 13.99, 17.4, 2.0, 3.0
-.align 16
-farr2:
-.float -214.0, 0.75, 5.5, 30.52
+    .align 16
+    farr1: .float 13.99, 17.4, 2.0, 3.0
+    .align 16
+    farr2: .float -214.0, 0.75, 5.5, 30.52
 .section .text
 .globl main
 .type main, @function
 main:
-# ...
-# calc 1. expression
-# ...
-# calc 2. expression
-# ...
-# calc 3. expression
-# ...
-# calc 4. expression
-# …
-# call truncDec
-leaq results, %rdi
-call truncDec
-# call printf
-leaq format, %rdi
-movq %rax, %rsi
-movq $0, %rax
-call printf
-# exit main
-movq $0, %rax
-popq %rbp
-ret
+    # move farr1 into %xmm0
+    vmovaps farr1, %xmm0
+    leaq farr1, %r9 # move array address of farr1 to %r9
+
+    # move farr2 into %xmm1
+    vmovaps farr2, %xmm1
+    leaq farr2, %r10 # move array address of farr2 into %10
+    
+    # calc 1. expression
+    vmovss (%r9), %xmm3
+    vsubps 4(%r9), %xmm3, %xmm0
+
+    # calc 2. expression
+
+    # calc 3. expression
+
+    # calc 4. expression
+
+    # call truncDec
+    leaq results, %rdi
+    call truncDec
+
+    # call printf
+    leaq format, %rdi
+    movq %rax, %rsi
+    movq $0, %rax
+    call printf
+
+    # exit main
+    movq $0, %rax
+    popq %rbp
+    ret
 .globl truncDec
 .type truncDec, @function
 truncDec:
-# …
-ret
+    ret
